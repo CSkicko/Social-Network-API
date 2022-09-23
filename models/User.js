7076d0338dd7e91003aa375fab335c0e13ba776b
Craig <craig.skicko@gmail.com>
2022-09-23 11:55:38 +1000

    Created basic seed file for users and fixed bugs in user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Schema, Types } = require('mongoose');
+const { Schema, Types, model } = require('mongoose');
 
 const userSchema = new Schema(
     {
@@ -42,6 +42,6 @@ userSchema
         return this.friends.length;
     });
 
-const User = model('user', userSchema);
+    const User = model('user', userSchema);
 
 module.exports = User;
---
+++ utils/seed.js
@@ -0,0 +1,21 @@
+const connection = require('../config/connection');
+const { User } = require('../models');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    await User.deleteMany({});
+
+    await User.insertMany([
+        {
+            username: 'test1',
+            email: 'test1@test.com',
+        },
+        {
+            username: 'test2',
+            email: 'test2@test.com',
+        }
+    ])
+    console.info('Seeding complete! 🌱');
+    process.exit(0);
+})
